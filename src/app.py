adbad20643414309f657fc19cbb0a5f1a6cc9df7
arrmansa <41120982+arrmansa@users.noreply.github.com>
2022-12-23 18:27:00 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,18 +1,9 @@
 from fastapi import FastAPI
 import logging
-from helpers.logging_uuid import logging_uuid
+from helpers.logging_uuid import change_logging
 from routers import test_router
 
-old_factory = logging.getLogRecordFactory()
-def record_factory(*args, **kwargs):
-    record = old_factory(*args, **kwargs)
-    record.uuid = logging_uuid.get("uuid")
-    return record
-logging.setLogRecordFactory(record_factory)
-
-logging.basicConfig(level=logging.INFO, 
-     format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s, %(uuid)s %(message)s',
-     datefmt='%H:%M:%S')
+change_logging()
 
 app = FastAPI( title="test_app", version="1.0")
 app.include_router(test_router.router)
