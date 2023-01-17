5f45be4664202dc26a0aa496db627110a4f0c73b
arrmansa <41120982+arrmansa@users.noreply.github.com>
2023-01-17 20:03:16 +0530

    Update logging_uuid.py
    faster

--- src/helpers/logging_uuid.py
+++ src/helpers/logging_uuid.py
@@ -10,10 +10,7 @@ def change_logging():
       record = old_factory(*args, **kwargs)
       record.uuid = logging_uuid.get("uuid")
       if isinstance(record.msg, str):
-          record.msg = re.sub(r"(\\*\n)|(\\+n)", lambda x: x.group()[:-1]*2 + ('n' if x.group()[-1]=='n' else '\\n'), record.msg)
-          # doubles number of \ appearing directly before n. eg '\\\n' -> '\\\\\\n'
-          # reverse using 
-          # record.msg = re.sub(r"(\\*\n)|(\\+n)", lambda x: '\\' * ((len(x.group())-1)//2) + ('n' if len(x.group())%2 else '\n'), record.msg)
+          record.msg = record.msg.replace("\\", "\\\\").replace("\n", "\\n")
       return record
   logging.setLogRecordFactory(record_factory)
   logging.basicConfig(level=logging.INFO, format= '%(asctime)s | {%(pathname)s:%(lineno)d} | %(levelname)s | %(uuid)s | %(message)s', force=True)
